35697d9cde400e6b9ab1b569335e176297d54159
Erik Carlsson <info@code301.com>
2015-01-09 11:36:27 +0100

    Init

---
+++ lib/CairoX/Sweet.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package CairoX::Sweet;
+
+# VERSION
+
+
+
+1;
+
+# ABSTRACT: Short intro
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use CairoX::Sweet;
+
+=head1 DESCRIPTION
+
+CairoX::Sweet is ...
+
+=head1 SEE ALSO
+
+=cut
