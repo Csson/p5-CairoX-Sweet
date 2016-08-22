fc5de29b12be400ab8522a60a5926db8a96a8637
Erik Carlsson <info@code301.com>
2016-08-22 19:44:00 +0000

    Add internal moose

---
+++ lib/CairoX/Sweet/Elk.pm
@@ -0,0 +1,29 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package CairoX::Sweet::Elk;
+
+# AUTHORITY
+# ABSTRACT: Internal Moose
+# VERSION
+
+use Moose();
+use MooseX::AttributeShortcuts();
+use namespace::autoclean();
+use List::Util();
+use Moose::Exporter;
+
+Moose::Exporter->setup_import_methods(also => ['Moose'], as_is => [\&List::Util::any, \&List::Util::sum, \&List::Util::zip]);
+
+sub init_meta {
+    my $class = shift;
+
+    my %params = @_;
+    my $for_class = $params{'for_class'};
+    Moose->init_meta(@_);
+    MooseX::AttributeShortcuts->init_meta(for_class => $for_class);
+    namespace::autoclean->import(-cleanee => $for_class);
+}
+
+1;
